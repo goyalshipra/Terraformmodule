11b3b35e6d4eb911e7cef75030bdce55c1d06613
goyalshipra <go.shipra@gmail.com>
2021-04-28 10:31:19 +0530

    first commit

---
+++ vpc-module/main.tf
@@ -0,0 +1,29 @@
+resource "aws_vpc" "myvpc" {
+  cidr_block = var.cidr_block
+  tags = {
+    Name = "amurthy_vpc"
+  }
+}
+
+resource "aws_subnet" "mysubnet" {
+  count = 2
+  vpc_id     = aws_vpc.myvpc.id
+  cidr_block = var.subnet_cidr[count.index]
+  tags = {
+    Name = "amurthy_subnet_${count.index + 1}"
+  }
+}
+
+resource "aws_instance" "myec2" {
+  count = 2
+  ami = var.ami_id
+  instance_type = var.instance_type
+  subnet_id = aws_subnet.mysubnet[count.index].id
+  tags = {
+      "Name" = var.ec2_tags[count.index]
+  }
+}
+
+output "myec2_instance_ip" {
+  value = aws_instance.myec2.*.private_ip
+}
